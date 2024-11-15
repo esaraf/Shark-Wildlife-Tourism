a62f6319f83fa7e29179a5dac13a32cdc37faafc
esaraf <elizabethsaraf@gmail.com>
2024-11-15 13:51:24 -0500

    Add requirements.txt and updated .gitignore

--- db_create.py
+++ db_create.py
@@ -4,7 +4,7 @@ from secrets_1 import USER, PASSWORD
 
 # Establish connection to the MySQL server 
 connection = mysql.connector.connect(
-    host='127.0.0.1',
+    host='localhost:3306',
     user=USER,
     password=PASSWORD,
     database='SWT'
