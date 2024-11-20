959670547baed9bdb9e2c38e3770e01013fc3d72
esaraf <elizabethsaraf@gmail.com>
2024-11-20 14:12:26 -0500

    Classified QuestionID by QuestionType

--- question_type_mapping.py
+++ question_type_mapping.py
@@ -1,3 +1,68 @@
 # A dictionary that maps question ids to question types 
-# Possible Options: 
-#       
+
+
+question_type_mapping = {
+    "Likert_Type_1": [ #Frequency Based
+        "1_1A",
+        "1_1B",
+        "1_1C",
+        "1_1D"
+    ], 
+    
+    "Likert_Type_2":[ #Agreement Based
+        "2_3A",
+        "2_3B",
+        "2_3C",
+        "2_3D",
+        "2_3E",
+        "2_3F",
+        "2_4A",
+        "2_4B",
+        "2_4C",
+        "2_4D",
+        "2_4E",
+        "2_4F",
+        "2_4G"
+    ], 
+
+    "MC":[ # Multiple Choice.... TO DO: Map MC QIDs to specific MC answers
+        "3_1A",
+        "3_2A",
+        "3_3A",
+        "3_4A",
+        "3_5A",
+        "3_6A"
+    ],
+
+    "Polar":[ # Yes / No
+        "1_3A",
+        "1_4A"
+    ],
+
+    "Scale":[ # 1 to 10 
+        "2_1A",
+        "2_2A",
+        "2_2B",
+        "2_2C",
+        "2_2D",
+        "2_1B",
+        "2_1C"
+    ],
+
+    "OPE":[ # Open Ended 
+        "1_1E",
+        "1_2E",
+        "1_3D",
+        "1_3B",
+        "2_2E",
+        "5_1A",
+        "5_2A",
+        "5_3A",
+        "5_4A"
+    ],
+
+    "ID":[ # Internal Survey ID
+        "0_0A"
+    ]
+}
+
